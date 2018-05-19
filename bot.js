5747ddafe78bd0f207238725e9ab75050f2d8638
Harris207Gamer <31186639+Harris207Gamer@users.noreply.github.com>
2018-05-19 16:45:55 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -61,21 +61,24 @@ if(cmd === `${prefix}kick`){
   
   let kUser = message.guild.member(message.mentions.users.first() ||  message.guild.members.get(args[0]));
   if(!kUser) return message.channel.send("ο χρήστης δεν βρέθηκε");
-  let Kreason = args.join(" ").slice(22);
+  let kReason = args.join(" ").slice(22);
   if(!message.member.hasPermission("KICK_MEMBERS")) return message.channel.send("δεν έχεις την άδεια να διώξεις αυτόν τον χρήστη");
   if(kUser.hasPermission("KICK_MEMBERS")) return message.channel.send("αυτός ο χρήστης δεν μπορέι να διωχτεί");
 
 
   let   KickEmbed = new Discord.RichEmbed()
-  .setTitle("~kick~")
+  .setTitle("~διώξιμο~")
   .setColor("33874")
   .addField("Διωχμένος χρήστης", `${kUser} ID: ${kUser.id}`)
   .addField("Τον έδιωξε ο", `<@${message.author.id}> ID: ${message.author.id}`)
   .addField("Διώχτικε στο channel", message.channel.createdAt)
   .addField("ώρα", message.createdAt)
-  .addField("λόγος", Kreason);
+  .addField("λόγος", kReason);
+  
+    let kickChannel = message.guild.channels.find(`name`, "αρχεία καταγραφής");
+  if(!kickChannel) return message.channel.send("δεν βρέθηκαν περιστατικά στο κανάλι");
 
-  message.guild.member(kUser).kick(reason);
+  message.guild.member(kUser).kick(kReason);
   kickChannel.send(KickEmbed);
 
   return;
@@ -85,19 +88,22 @@ if(cmd === `${prefix}kick`){
 
   let bUser = message.guild.member(message.mentions.users.first() ||  message.guild.members.get(args[0]));
   if(!bUser) return message.channel.send("ο χρήστης δεν βρέθηκε");
-  let breason = args.join(" ").slice(22);
+  let bReason = args.join(" ").slice(22);
   if(!message.member.hasPermission("BAN_MEMBERS")) return message.channel.send("δεν έχεις την άδεια να αποκλείσεις αυτόν τον χρήστη");
   if(bUser.hasPermission("BAN_MEMBERS")) return message.channel.send("αυτός ο χρήστης δεν μπορέι να αποκλειστεί");
 
 
   let   BanEmbed = new Discord.RichEmbed()
-  .setTitle("~ban~")
+  .setTitle("~αποκλεισμός~")
   .setColor("73484")
   .addField("Αποκλεισμένος χρήστης", `${bUser} ID: ${bUser.id}`)
   .addField("Τον Απόκλεισε ο", `<@${message.author.id}> ID: ${message.author.id}`)
   .addField("Αποκλείστηκε στο channel", message.channel.createdAt)
   .addField("ώρα", message.createdAt)
-  .addField("λόγος", breason);
+  .addField("λόγος", bReason);
+  
+    let banChannel = message.guild.channels.find(`name`, "αρχεία καταγραφής");
+  if(!banChannel) return message.channel.send("δεν βρέθηκαν περιστατικά στο κανάλι");
   
   message.guild.member(bUser).ban(bReason);
   banChannel.send(banEmbed);
