4015b19f5696d64e5589f2768b3d38116b60ab8b
Harris207Gamer <31186639+Harris207Gamer@users.noreply.github.com>
2018-06-23 16:06:22 +0300

    Add files via upload

---
+++ commands/addrole.js
@@ -0,0 +1,18 @@
+const Discord = require("discord.js");
+
+module.exports.run = async (bot, message, arga) => {
+  if(!message.member.hasPermission("MANAGE_MEMBERS")) return message.reply("αυτός ο χρήστης δεν είναι δυνατόν να πρεσθεθεί ρόλος");
+  let rMember = message.guild.member(message.mentions.users.first()) || message.guild.members.get(args[0]);
+   if(!rMember) return message.reply("ο χρήστης δεν βρέθηκε");
+   let role = args.join(" ").slice(22);
+   if(!role) return message.reply("βάλε έναν ρόλο");
+   let gRole = message.guild.roles.find(`name`, role);
+   if(!gRole) return message.reply("δεν βρέθηκε ρόλος.");
+
+   if(rMember.roles.has(gRole.id)) return message.reply("αυτός ο χρήστης έχει ήδη αυτόν τον ρόλο.");
+   await(rMember.addRole(gRole.id));
+ }
+
+ module.exports.help = {
+   name: "addrole"
+ }
---
+++ index.js
@@ -0,0 +1,53 @@
+const botconfig = require("./botconfig.json");
+const tokenfile = require("./token.json");
+const Discord = require("discord.js");
+const fs = require("fs");
+const bot = new Discord.Client({disableEveryone: true});
+bot.commands = new Discord.Collection();
+
+fs.readdir("./commands/", (err, files) => {
+
+  if(err) console.log(err);
+  let jsfile = files.filter(f => f.split(".").pop() === "js")
+  if(jsfile.length <= 0){
+    console.log("δεν βρέθηκαν εντολές.");
+    return;
+  }
+
+  jsfile.forEach((f, i) =>{
+    let props = require(`./commands/${f}`);
+    console.log(`${f} loaded!`);
+    bot.commands.set(props.help.name, props);
+  });
+});
+
+bot.on("ready", async () => {
+  console.log(`${bot.user.username} is online on ${bot.guilds.size} servers!`);
+  bot.user.setActivity("στις εντολές σας", {type: "LISTENING"});
+  bot.user.setStatus("online");
+    // bot.user.setStatus("idle");
+    //   bot.user.setStatus("dnd");
+    //     bot.user.setStatus("invisible");
+
+
+});
+
+bot.on("message", async message => {
+  if(message.author.bot) return;
+  if(message.channel.type === "dm") return;
+
+  let prefix = botconfig.prefix;
+  let messageArray = message.content.split(" ");
+  let cmd = messageArray[0];
+  let args = messageArray.slice(1);
+  let commandfile = bot.commands.get(cmd.slice(prefix.length));
+  if(commandfile) commandfile.run(bot,message,args);
+
+  -if (message.content === "fuck"){
+  -    message.react("😠");
+  -}
+
+
+});
+
+bot.login(tokenfile.token);
