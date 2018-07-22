76f8847c7520bded163fc58fc5d595e19227e1dc
Harris207Gamer <31186639+Harris207Gamer@users.noreply.github.com>
2018-07-23 02:44:18 +0300

    Update addrole.js

--- commands/addrole.js
+++ commands/addrole.js
@@ -3,15 +3,15 @@ const Discord = require("discord.js");
 module.exports.run = async (bot, message, args) => {
 
   //!addrole @andrew Dog Person
-  if(!message.member.hasPermission("MANAGE_MEMBERS")) return message.reply("Sorry pal, you can't do that.");
+  if(!message.member.hasPermission("MANAGE_MEMBERS")) return message.reply("Συγνώμη αλλά δεν μπορώ να το κάνω.");
   let rMember = message.guild.member(message.mentions.users.first()) || message.guild.members.get(args[0]);
-  if(!rMember) return message.reply("Couldn't find that user, yo.");
+  if(!rMember) return message.reply("Ο χρήστης δεν βρέθηκε.");
   let role = args.join(" ").slice(22);
   if(!role) return message.reply("Specify a role!");
   let gRole = message.guild.roles.find(`name`, role);
-  if(!gRole) return message.reply("Couldn't find that role.");
+  if(!gRole) return message.reply("Ο ρόλος δεν βρέθηκε.");
 
-  if(rMember.roles.has(gRole.id)) return message.reply("They already have that role.");
+  if(rMember.roles.has(gRole.id)) return message.reply("Αυτός ο χρήστης έχει ήδη αυτον τον ρόλο.");
   await(rMember.addRole(gRole.id));
 
   try{
@@ -19,7 +19,10 @@ module.exports.run = async (bot, message, args) => {
   }catch(e){
     message.channel.send(`Congrats to <@${rMember.id}>, they have been given the role ${gRole.name}. We tried to DM them, but their DMs are locked.`)
   }
-}
+}  
+  try{
+    await message.channel.send("`Ο ρόλος άλλαξε ☑️`")
+    }
 
 module.exports.help = {
   name: "addrole"
