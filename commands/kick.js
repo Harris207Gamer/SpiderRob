271df08b8d2776a038776578a8d2798619b2c134
Harris207Gamer <31186639+Harris207Gamer@users.noreply.github.com>
2018-07-23 02:53:03 +0300

    Update kick.js

--- commands/kick.js
+++ commands/kick.js
@@ -4,17 +4,17 @@ module.exports.run = async (bot, message, args) => {
     let kUser = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
     if(!kUser) return message.channel.send("Can't find user!");
     let kReason = args.join(" ").slice(22);
-    if(!message.member.hasPermission("MANAGE_MESSAGES")) return message.channel.send("No can do pal!");
-    if(kUser.hasPermission("MANAGE_MESSAGES")) return message.channel.send("That person can't be kicked!");
+    if(!message.member.hasPermission("MANAGE_MESSAGES")) return message.channel.send("Δεν μπορώ να το κάνω!");
+    if(kUser.hasPermission("MANAGE_MESSAGES")) return message.channel.send("Αυτό το άτομο δεν μπορεί να διωχτεί!");
 
     let kickEmbed = new Discord.RichEmbed()
-    .setDescription("~Kick~")
+    .setDescription("~Αποχώρηση~")
     .setColor("#e56b00")
-    .addField("Kicked User", `${kUser} with ID ${kUser.id}`)
-    .addField("Kicked By", `<@${message.author.id}> with ID ${message.author.id}`)
-    .addField("Kicked In", message.channel)
-    .addField("Tiime", message.createdAt)
-    .addField("Reason", kReason);
+    .addField("Χρήστης που διώχτηκε", `${kUser} with ID ${kUser.id}`)
+    .addField("Διώχτηκε από", `<@${message.author.id}> with ID ${message.author.id}`)
+    .addField("Διώχτικε στο", message.channel)
+    .addField("Ώρα που διώχτηκε", message.createdAt)
+    .addField("Λόγος", kReason);
 
     let kickChannel = message.guild.channels.find(`name`, "incidents");
     if(!kickChannel) return message.channel.send("Can't find incidents channel.");
