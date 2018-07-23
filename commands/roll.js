efbaeaedbecdf9a2bd976dfcf999082cbe992db6
Harris207Gamer <31186639+Harris207Gamer@users.noreply.github.com>
2018-07-23 05:00:12 +0300

    Create roll.js

---
+++ commands/roll.js
@@ -0,0 +1,14 @@
+const Discord = require("discord.js");
+
+module.exports.run = async (bot, message, args) => {
+
+var y = Math.floor(Math.random() * 6 + 1);
+var x = Math.floor(Math.random() * 6 + 1);
+
+
+ message.channel.send("έφερες"+"```"+(y)+"+"+(x)+(" = ")+(y+x)+"```");
+
+}
+module.exports.help = {
+  name: "addrole"
+}
