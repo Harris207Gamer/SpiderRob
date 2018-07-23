00e25e6a84216a4e474551778dd991d55c42232d
Harris207Gamer <31186639+Harris207Gamer@users.noreply.github.com>
2018-07-23 13:19:59 +0300

    Update addrole.js

--- commands/addrole.js
+++ commands/addrole.js
@@ -3,10 +3,10 @@ const Discord = require("discord.js");
 module.exports.run = async (bot, message, args) => {
 
   //!addrole @andrew Dog Person
-  if(!message.member.hasPermission("MANAGE_MEMBERS")) return message.reply("Συγνώμη αλλά δεν μπορώ να το κάνω.");
+  if(!message.member.hasRole("🛡ΔΙΑΧΕΙΡΗΣΤΕΣ🛡")) return message.reply("Συγνώμη αλλά δεν μπορώ να το κάνω.");
   let rMember = message.guild.member(message.mentions.users.first()) || message.guild.members.get(args[0]);
   if(!rMember) return message.reply("Ο χρήστης δεν βρέθηκε.");
-  let role = args.join(" ").slice(22);
+  let role = args.join("@"+" ").slice(22);
   if(!role) return message.reply("Διάλεξε έναν ρόλο!");
   let gRole = message.guild.roles.find(`name`, role);
   if(!gRole) return message.reply("Ο ρόλος δεν βρέθηκε.");
