029c6a11ecd0fb1c146c59d6e83190daf3f2f334
Harris207Gamer <31186639+Harris207Gamer@users.noreply.github.com>
2018-07-23 13:50:25 +0300

    Update removerole.js

--- commands/removerole.js
+++ commands/removerole.js
@@ -1,11 +1,11 @@
 const Discord = require("discord.js");
 
 module.exports.run = async (bot, message, args) => {
-  let admin = message.guild.roles.find("name", "🛡ΔΙΑΧΕΙΡΗΣΤΕΣ🛡");
+  let admin = message.guild.roles.find("name", "@🛡ΔΙΑΧΕΙΡΗΣΤΕΣ🛡");
   if(!message.member.roles.has(admin)) return message.reply("αυτός ο χρήστης δεν είναι δυνατόν να πρεσθεθεί ρόλος");
   let rMember = message.guild.member(message.mentions.users.first()) || message.guild.members.get(args[0]);
   if(!rMember) return message.reply("ο χρήστης δεν βρέθηκε");
-  let role = args.join(" ").slice(22);
+  let role = args.join("@"+" ").slice(22);
   if(!role) return message.reply("βάλε έναν ρόλο");
   let gRole = message.guild.roles.find(`name`, role);
   if(!gRole) return message.reply("δεν βρέθηκε ρόλος.");
