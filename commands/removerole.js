f3bdfc4651fddae1aec26cff3fb2cb9af810261f
Harris207Gamer <31186639+Harris207Gamer@users.noreply.github.com>
2018-07-23 04:37:59 +0300

    Update removerole.js

--- commands/removerole.js
+++ commands/removerole.js
@@ -13,7 +13,7 @@ module.exports.run = async (bot, message, args) => {
   await(rMember.removeRole(gRole.id));
 
   try {
-    await message.channel.send("`Ο ρόλος αφερέθηκε`❎")
+    await message.channel.send(`Ο ρόλος αφερέθηκε❎`)
   }
   
   }
