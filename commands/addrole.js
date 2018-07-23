7082c741b51e14361a93887253fadf4f2303b2e4
Harris207Gamer <31186639+Harris207Gamer@users.noreply.github.com>
2018-07-23 03:42:47 +0300

    Update addrole.js

--- commands/addrole.js
+++ commands/addrole.js
@@ -7,22 +7,13 @@ module.exports.run = async (bot, message, args) => {
   let rMember = message.guild.member(message.mentions.users.first()) || message.guild.members.get(args[0]);
   if(!rMember) return message.reply("Ο χρήστης δεν βρέθηκε.");
   let role = args.join(" ").slice(22);
-  if(!role) return message.reply("Specify a role!");
+  if(!role) return message.reply("Διάλεξε έναν ρόλο!");
   let gRole = message.guild.roles.find(`name`, role);
   if(!gRole) return message.reply("Ο ρόλος δεν βρέθηκε.");
 
   if(rMember.roles.has(gRole.id)) return message.reply("Αυτός ο χρήστης έχει ήδη αυτον τον ρόλο.");
   await(rMember.addRole(gRole.id));
 
-  try{
-    await rMember.send(`Congrats, you have been given the role ${gRole.name}`)
-  }catch(e){
-    message.channel.send(`Congrats to <@${rMember.id}>, they have been given the role ${gRole.name}. We tried to DM them, but their DMs are locked.`)
-  }
-
-  try{
-    await message.channel.send(`Ο ρόλος προσθέθηκε ☑️ `)
-    }
 
 module.exports.help = {
   name: "addrole"
