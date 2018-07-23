ee8871dc5c8ee9ca782771ef8a8ff5c2ce8c5b5b
Harris207Gamer <31186639+Harris207Gamer@users.noreply.github.com>
2018-07-23 04:11:22 +0300

    Update roll.js

--- commands/roll.js
+++ commands/roll.js
@@ -4,9 +4,10 @@ module.exports.run = async (bot, message, args) => {
  var y = Math.floor(Math.random() * 6 + 1);
 var x = Math.floor(Math.random() * 6 + 1);
 
-
+try {
   message.channel.send("έφερες"+"```"+(y)+"+"+(x)+(" = ")+(y+x)+"```");
-
+}
+ 
 }
 
 module.exports.help = {
