fe6e0de11056a1d0386f6efb856ac52eab264e21
Harris207Gamer <31186639+Harris207Gamer@users.noreply.github.com>
2018-07-25 01:00:44 +0300

    Update and rename math to math.js

--- commands/math.js
+++ commands/math.js
@@ -1,16 +1,16 @@
 const Discord = require("discord.js");
 const scalc = requite("scalc");
 
-module.exports.run = (bot, message ,args){
+module.exports.run = (bot, message ,args) => {
 
-{
+
 	let calc = scalc(message.content.split(" ").slice(1));
 	//let result = scalc(`${calc}`);
 	if (!calc) return message.reply("διάλεξε πράξη");
   //if (!result) return message.reply("δεν βγαίνει αποτέλεσμα");
   //console.log(result);
 	return message.channel.send(calc);
-	}
+	
 
 }
 
