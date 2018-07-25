fc91737e9cb1e987d9f66664a3c2a4ec24ca876e
Harris207Gamer <31186639+Harris207Gamer@users.noreply.github.com>
2018-07-25 16:06:58 +0300

    Update math.js

--- commands/math.js
+++ commands/math.js
@@ -4,12 +4,11 @@ const scalc = require("scalc");
 module.exports.run = async (bot, message ,args) => {
 
         
+	let math = args.join(" ");
+	var result = scalc(`${math}`);
 	
-	let calc = args.join(" ");
-	let result = scalc(`${calc}`);
-	if (!calc) return message.reply("διάλεξε πράξη");
-        if (!result) return message.channel.send("0");
-
+        if (!math) return message.reply("διάλεξε πράξη");
+        console.log(result);
 	return message.channel.send(result);
   
 	
