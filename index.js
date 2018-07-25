c70c2f1bc797af0c953639d8b596f8e17655686e
Harris207Gamer <31186639+Harris207Gamer@users.noreply.github.com>
2018-07-25 16:43:28 +0300

    Update index.js

--- index.js
+++ index.js
@@ -39,7 +39,7 @@ bot.on("message", async message => {
 
   let prefix = botconfig.prefix;
   let messageArray = message.content.split(" ");
-  let command = messageArray[0];
+  let cmd = messageArray[0];
   let args = messageArray.slice(1);
   let commandfile = bot.commands.get(cmd.slice(prefix.length));
   if(commandfile) commandfile.run(bot,message,args);
