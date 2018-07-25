972aeb7ffebb10a0cf18adb6ef7c7c12f2fe3609
Harris207Gamer <31186639+Harris207Gamer@users.noreply.github.com>
2018-07-25 12:53:41 +0300

    Update math.js

--- commands/math.js
+++ commands/math.js
@@ -1,9 +1,10 @@
 const Discord = require("discord.js");
 const scalc = require("scalc");
 
-module.exports.run = (bot, message ,args) => {
+module.exports.run = (bot, message ,args, prefix , command) => {
 
-
+        if (command === prefix+"math"){
+	
 	let calc = message.content.split(" ").slice(1);
 	let result = scalc(`${calc}`);
 	if (!calc) return message.reply("διάλεξε πράξη");
@@ -15,6 +16,6 @@ module.exports.run = (bot, message ,args) => {
 
 }
 
-module.exports.help = {
-    name:"math"
-}
+//module.exports.help = {
+  //  name:"math"
+//}
