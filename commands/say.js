78057ec21a6e98d942ad044a124dec9e6d631c5d
Harris207Gamer <31186639+Harris207Gamer@users.noreply.github.com>
2018-07-27 14:44:28 +0300

    Update say.js

--- commands/say.js
+++ commands/say.js
@@ -2,6 +2,8 @@ const Discord = require("discord.js");
 
 module.exports.run = async (bot, message, args) => {
  let say = args.join(" ");
+ if(!say) return message.reply("πες κάτι");
+ message.delete().catch();
  return message.channel.send(say);
 }
 
