73eed741cc50b12419007cb4b22301ae945c9e55
Harris207Gamer <31186639+Harris207Gamer@users.noreply.github.com>
2018-07-27 16:59:29 +0300

    Update edit.js

--- commands/edit.js
+++ commands/edit.js
@@ -2,34 +2,31 @@ const Discord = require("discord.js");
 
 module.exports.run = async (message, bot, args) => {
 
-	var admin = message.guild.roles.get("381016423612416000");
-	
-	
-if(!message.member.roles.has(admin.id)) return message.reply("δεν έχεις τον ρόλο για να το κάνεις αυτό");
-	
-	var bot_edit = args.join(" ").slice(41);
-	var channel_id_get = args.join(" ").slice(2,20);
-	var message_id = args.join(" ").slice(22,40);
-	
-if (!channel_id_get) return message.reply("Διάλεξε channel");
-	
+var admin = message.guild.roles.get("381016423612416000");		
+if(!message.member.roles.has(admin.id)) return message.reply("δεν έχεις τον ρόλο για να το κάνεις αυτό");		
+
+
+
+var bot_edit = args.join(" ").slice(41);	
+var channel_id_get = args.join(" ").slice(2,20);
+
+var message_id = args.join(" ").slice(22,40);	
+
+if (!channel_id_get) return message.reply("Διάλεξε channel");	
 if (!message_id) return message.reply("Διάλεξε id μηνύματος");
 if (!bot_edit) return message.reply("Πές κάτι");
 
-	var channel_id = message.guild.channels.get(channel_id_get);
-	
-	message.delete().catch();
-	   channel_id.fetchMessage(message_id).then((message) => {
-		message.edit(bot_edit)
-		
-	});
-		
+ 	var channel_id = message.guild.channels.get(channel_id_get);		
 
-	
-	
+message.delete().catch();	
+ channel_id.fetchMessage(message_id).then((message) => {		
+	 message.edit(bot_edit)			
 
-
+});
 }
+
+
+
 
 module.exports.help = {
     name: "edit"
