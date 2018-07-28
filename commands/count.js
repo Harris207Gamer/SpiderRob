7f5f57147613ab1be7357ae8195f06af2ac73f92
Harris207Gamer <31186639+Harris207Gamer@users.noreply.github.com>
2018-07-28 12:25:44 +0300

    Create count.js

---
+++ commands/count.js
@@ -0,0 +1,46 @@
+const Discord = require("discord.js");
+
+module.exports.run = async (bot, message, args) => {
+
+
+	
+  var firstnumberget = args.join(" ").slice(0,8);
+	var secondnumberget = args.join(" ").slice(9,17);
+  var firstnumber = scalc(`${firstnumberget}`);
+	var secondnumber = scalc(`${secondnumberget}`);
+	
+var count = firstnumber;
+	
+	
+	
+console.log("from "+firstnumber);
+console.log("to "+secondnumber);
+
+
+var clear = setInterval(function(){
+	
+	
+	
+	
+count++;  
+console.log(count);
+if (count === secondnumber){
+	clearInterval(clear)
+}},1000)
+  
+ 
+  
+ message.channel.send(firstnumber).then((message) => {
+  	  var intId = setInterval(counter,1000);
+  	  function counter(){message.edit(count)};
+  	  
+    })
+  	     
+  	
+  
+
+}
+
+module.exports.help = {
+   name : "count"
+}
